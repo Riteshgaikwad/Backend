3daa0d83e55b607887585da608ee1f2a739fe01d
Riteshgaikwad <gaikwadritesh2003@gmail.com>
2024-09-20 19:20:14 +0530

    dome

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+require('dotenv').config()
+
+
+const port = process.env.PORT
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/twitter',(req,res)=>{
+    res.send('riteshgaikwad')
+})
+app.get('/login',(req,res)=>{
+    res.send('<h1>Please login at chai aur code</h1>')
+})
+app.get('/youtube',(req,res)=>{
+    res.send('<h2>chaiiiii</h2>')
+})
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
